c85591abf6c0e762906e335d08cc9647b8aab68a
Shashankdk23 <117855805+Shashankdk23@users.noreply.github.com>
2022-12-15 15:43:06 +0530

    Add files via upload

--- week6.java
+++ week6.java
@@ -1,94 +1,158 @@
-/*Develop a Java program to create a class Bank that maintains two kinds of account for its customers,
-one called savings account and the other current account. The savings account provides compound
-interest and withdrawal facilities but no cheque book facility. The current account provides cheque
-book facility but no interest. Current account holders should also maintain a minimum balance and if
-the balance falls below this level, a service charge is imposed.
-Create a class Account that stores customer name, account number and type of account. From this
-derive the classes Cur-acct and Sav-acct to make them more specific to their requirements. Include the
-necessary methods in order to achieve the following tasks.
-a) Accept deposit from customer and update the balance.
-b) Display the balance.
-c) Compute and deposit interest.
-d) Permit withdrawal and update the balance.
-Check for the minimum balance, impose penalty if necessary and update the balance */
-
-/*////////////////////////////////////////////////////////////////////////////*/
 import java.util.Scanner;
 
+class Account {
+    Scanner s = new Scanner(System.in);
+    public String name;
+    long accountnum;
+    int accountType;
+    
+}
 
-class bank{
+class currentAccount extends Account {
+    double penaltyPercent = 0.08;
+    double balance = 0;
 
+    void creation() {
+        System.out.println("enter your name");
+        name = s.nextLine();
+        System.out.println("enter account number");
+        accountnum = s.nextLong();
+        System.out.println("enter amount greater than 5000 rupees");
+        balance = s.nextDouble();
+    }
+    void balance() {
+        if (balance < 1000) {
+            Double penalty = balance * penaltyPercent;
+            balance -= penalty;
+            System.out.println("penalty amount reduced because of low balance " + penalty + " rupees");
+        }
+        System.out.println("your account balance is " + balance);
+    }
 
+    void chequeBook() {
+        System.out.println("you can avail chequeBook");
+    }
 
-}
-abstract class account
-{ 
-  Scanner s=new Scanner(System.in);
-  String name,acc_no,type;
-  double balance=0;
-  account(String n,String a,String t)
-  {
-    name=n;
-    acc_no=a;
-    type=t;
-  }
-abstract void deposit();
-abstract void display();
-abstract void withdrawal();
-}
-class saving extends account
-{
- void deposit()
-  {
-   System.out.println("Enter the amount to deposit");
-   double d=s.nextDouble();
-  balance+=d;
-  }
-void withdrawal()
- {
-  System.out.println("Enter the amount to Withdraw");
-   double w=s.nextDouble();
-  balance-=w;
-  }
-void display()
-{
- System.out.println("The account no is : "+acc_no);
- double b=.06;
-System.out.println("The balance is : "+balance);
-  double interest=balance*b;
-System.out.println("The balance is : "+interest);
-     balance=balance+interest;
- System.out.println("The net balance is : "+balance);
-}
- saving(String n,String a,String t)
- {
-  super(n,a,t);
- int flag=1;
+    void deposit() {
+        System.out.println("enter amount you want to deposit");
+        balance += s.nextDouble();
+    }
 
-  System.out.println("Enter the choice 1.deposit  2.withdrawal 3.diplay balance 4.exit");
- int ch=s.nextInt();
-while(flag==1)
-{
- switch(ch)
-  {
-   case'1':deposit();
-           break;
-  case'2':withdrawal();
-          break;
-  case'3':display();
-          break;
-  case'4':flag=0;
-break;
- 
-  }
-  }
+    void withdraw() {
+        if (balance < 1000) {
+            System.out.println("your account balance is lower than 1000 rupees so you can't withdraw");
+        } else {
+            System.out.println("enter amount you want to withdraw");
+            balance -= s.nextDouble();
+        }
+        balance();
+    }
 }
+
+class savingAccount extends Account {
+    double interest = .06;
+    int year;
+    double balance = 0;
+
+    void creation() {
+        System.out.println("enter your name");
+        name = s.nextLine();
+        System.out.println("enter account number");
+        accountnum = s.nextLong();
+        System.out.println("enter amount greater than 5000 rupees");
+        balance = s.nextDouble();
+    }
+    void chequeBook() {
+        System.out.println("you can't avail chequeBook");
+    }
+
+    void balance() {
+       // System.out.println("enter the year passed after deposition of amount");
+        // year = s.nextInt();
+        balance += (balance * interest );
+        System.out.println("your account balance is " + balance);
+    }
+
+    void deposit() {
+        System.out.println("enter amount you want to deposit");
+        balance += s.nextDouble();
+    }
+
+    void withdraw() {
+        if (balance < 1000) {
+            System.out.println("your account balance is lower than 1000 rupees so you can't withdraw");
+        } else {
+            System.out.println("enter amount you want to withdraw");
+            balance -= s.nextDouble();
+        }
+        balance();
+    }
 }
-class week
- {
-   public static void main(String args[])
-  {
-    saving n=new saving("shree","5634","save");
- }
+
+ class bank {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter account type you want to have 1 for currentAccount\n2 for savingAccount");
+        int n = s.nextInt();
+        if (n == 1) {
+            currentAccount c = new currentAccount();
+            c.creation();
+            Boolean b = true;
+            while (b) {
+                System.out.println("enter 1 for deposit\n2 for withdraw\n3 for balance\n4 for cheque book\n5 to exit");
+                int ch = s.nextInt();
+                switch (ch) {
+                    case 1:
+                        c.deposit();
+                        break;
+                    case 2:
+                        c.withdraw();
+                        break;
+                    case 3:
+                        c.balance();
+                        break;
+                    case 4:
+                        c.chequeBook();
+                        break;
+                    case 5:
+                        System.out.println("Successfully logged out of your account");
+                        b = false;
+                        break;
+                    default:
+                        System.out.println("invalid input");
+                        break;
+                }
+            }
+        } else {
+            Boolean b = true;
+            savingAccount sa = new savingAccount();
+            sa.creation();
+            while (b) {
+                System.out.println("enter 1 for deposit\n2 for withdraw\n3 for balance\n4 for cheque book\n5 to exit");
+                int ch = s.nextInt();
+                switch (ch) {
+                    case 1:
+                        sa.deposit();
+                        break;
+                    case 2:
+                        sa.withdraw();
+                        break;
+                    case 3:
+                        sa.balance();
+                        break;
+                    case 4:
+                        sa.chequeBook();
+                        break;
+                    case 5:
+                        System.out.println("Successfully logged out of your account");
+                        b = false;
+                        break;
+                    default:
+                        System.out.println("invalid input");
+                        break;
+                }
+            }
+        }
+        s.close();
+    }
 }
-   
